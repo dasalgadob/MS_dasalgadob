29683fd13961f4e98a58397b9e359447aea6edfc
Diego Salgado <dasalgadob@unal.edu.co>
2018-10-12 16:35:52 -0500

    Docker working

--- app.js
+++ app.js
@@ -43,7 +43,7 @@ app.use(function(err, req, res, next) {
 });
 
 var mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost/crimesdb');
+mongoose.connect('mongodb://mongo:27017/crimesdb');
 
 //import model
 var Crime = require('./model/crime');
